5bd8ab229bca02bcf2606d95ec9d55f4111228c6
Mike <36743998+wudoem@users.noreply.github.com>
2020-08-18 13:44:51 +0200

    Update Router.tsx

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -9,7 +9,7 @@ export const Router: React.FunctionComponent<RouterProps> = () => {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
-      <Route exact path="/list" component={ListPage} />
+      <Route exact path="/top100" component={ListPage} />
 
       <Route component={() => <Redirect to="/" />} />
     </Switch>
